361d0ef2177f5271a5ea2744ea546f3eaf722b8c
Koji Sudo <higedrum.coz@gmail.com>
2016-06-14 00:35:49 +0900

    locationをレスポンスヘッダに乗せるPOST実装

--- src/main/java/com/higedrum/springboot/rest/sample/api/CustomerRestController.java
+++ src/main/java/com/higedrum/springboot/rest/sample/api/CustomerRestController.java
@@ -3,9 +3,13 @@ package com.higedrum.springboot.rest.sample.api;
 import com.higedrum.springboot.rest.sample.domain.Customer;
 import com.higedrum.springboot.rest.sample.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 /**
@@ -20,6 +24,8 @@ public class CustomerRestController {
     CustomerService customerService;
 
     /**
+     * curl http://localhost:8080/api/customers/ -v -X GET
+     *
      * @return 顧客全件取得
      */
     @RequestMapping(method = RequestMethod.GET)
@@ -29,6 +35,8 @@ public class CustomerRestController {
     }
 
     /**
+     * curl http://localhost:8080/api/customers/129 -v -X GET
+     *
      * @param id 顧客ID
      * @return 顧客一件取得
      */
@@ -38,14 +46,30 @@ public class CustomerRestController {
         return customer;
     }
 
+//    /**
+//     * @param customer 顧客
+//     * @return 顧客１件登録
+//     */
+//    @RequestMapping(method = RequestMethod.POST)
+//    @ResponseStatus(HttpStatus.CREATED)
+//    Customer postCustomers(@RequestBody Customer customer) {
+//        return customerService.create(customer);
+//    }
+
     /**
+     * レスポンスヘッダに作成した顧客のlocationを付与して返却する
+     * <p>curl http://localhost:8080/api/customers/ -v -X POST -H "Content-Type: application/json" -d "{\"firstName\" : \"Taro\", \"lastName\" : \"Yamada\"}"</p>
+     *
      * @param customer 顧客
      * @return 顧客１件登録
      */
     @RequestMapping(method = RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    Customer postCustomers(@RequestBody Customer customer) {
-        return customerService.create(customer);
+    ResponseEntity<Customer> postCustomers(@RequestBody Customer customer, UriComponentsBuilder uriComponentsBuilder) {
+        Customer created = customerService.create(customer);
+        URI location = uriComponentsBuilder.path("api/customers/{id}").buildAndExpand(created.getId()).toUri();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setLocation(location);
+        return new ResponseEntity<>(created, headers, HttpStatus.CREATED);
     }
 
     /**
@@ -61,7 +85,7 @@ public class CustomerRestController {
 
     @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
     @ResponseStatus(HttpStatus.NO_CONTENT)
-    void deleteCustomer(@PathVariable Integer id){
+    void deleteCustomer(@PathVariable Integer id) {
         customerService.delete(id);
     }
 }
